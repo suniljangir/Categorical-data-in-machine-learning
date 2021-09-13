9986b51bcb9a4113ced25707a3f06474eb838ca4
jangirjecrc <55182236+jangirjecrc@users.noreply.github.com>
2021-09-13 08:50:33 +0530

    Add files via upload

---
+++ Suni Jangir/DataTypeDemo.java
@@ -0,0 +1,24 @@
+class DataTypeDemo{
+public static void main(String a[]){
+   		byte b	=    1;
+short s	=    300;
+int i     	=    123123;
+long l   	=    65467890987L;
+
+float f  	=     3.1416256f;
+double d	=     1222.0235859256566;
+char c	=    'a';
+boolean bln	=    true;
+
+System.out.println("byte b is "+b);
+System.out.println("short s is "+s);
+System.out.println("int i is "+i);
+System.out.println("long l is "+l);
+
+System.out.println("float f is "+f);
+System.out.println("double d is "+d);
+
+System.out.println("char c is "+c);
+System.out.println("boolean bln is "+bln);
+    	}
+}
